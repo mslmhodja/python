f26801861ffc8adba8d119b7f77d076a16d4c8e7
Muslim HODJA <85079865+mslmhodja@users.noreply.github.com>
2024-11-20 15:35:37 +0300

    Update uygulama-25.py

--- uygulama-25.py
+++ uygulama-25.py
@@ -1,6 +1,9 @@
 # bir cümlede kaç tane a harfi olduğunu sayan kod.
+# . yı görünce dur.
 sayac=0
-for x in "banana":
-  if x=="a":
+for x in "bir cümlede kaç tane a harfi olduğunu sayan kod. bir cümlede kaç tane a harfi olduğunu sayan kod.":
+  if x=="r":
     sayac+=1
+  if x==".":
+    break
 print (sayac, "defa")
