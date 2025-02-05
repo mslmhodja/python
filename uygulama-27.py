df48e14fbdafec2acca823028fce4633fb93dae2
Muslim HODJA <85079865+mslmhodja@users.noreply.github.com>
2025-02-05 12:31:40 +0300

    Update uygulama-27.py

--- uygulama-27.py
+++ uygulama-27.py
@@ -1,1 +1,19 @@
-# boş
+# argümansız fomksiyon tanımlama
+def Selamla():
+  print("Selamün aleyküm gardaş")
+
+Selamla()
+
+# argümansız fomksiyon tanımlama , dışarıdanalınan bilgiyi kullnama
+def kareBul():
+  global sayi
+  print(sayi*sayi)
+
+sayi = 5
+Karebul()
+
+# argümanlı fomksiyon tanımlama , dışarıdanalınan bilgiyi kullnama
+def kareBul(sayi):
+  print(sayi*sayi)
+
+kareBul(5)
