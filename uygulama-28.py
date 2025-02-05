99e29ca8e2f77801d7624a4df409ea74f2ebbf68
Muslim HODJA <85079865+mslmhodja@users.noreply.github.com>
2025-02-05 15:53:49 +0300

    Update uygulama-28.py

--- uygulama-28.py
+++ uygulama-28.py
@@ -21,3 +21,14 @@ def yildiz(n):
      yildiz(n-1)
 
 yildiz(15)
+
+
+# 1 den verilen sayıya kadarolan tüm sayıların toplanını bulan rekürsif fonksiyon
+
+def n_toplam(n):
+   if n==1: 
+     return 1
+   else:
+     return n + n_toplam(n-1) 
+
+print(n_toplam(5))
