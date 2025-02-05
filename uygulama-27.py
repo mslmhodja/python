dfa0c1b35a340bb1ef3165a2a7533bc6c36fec86
Muslim HODJA <85079865+mslmhodja@users.noreply.github.com>
2025-02-05 13:27:14 +0300

    Update uygulama-27.py

--- uygulama-27.py
+++ uygulama-27.py
@@ -17,3 +17,18 @@ def kareBul(sayi):
   print(sayi*sayi)
 
 kareBul(5)
+
+# çok argümanlı fonksiyon tanımlama , iki sayının ortalaması
+def OrtalamaBul(sayi1,sayi2):
+  ortalama = (sayi1+sayi2)/2
+  print(ortalama)
+
+OrtalamaBul(90,70)
+
+# çok argümanlı fonksiyon tanımlama , argüman sayısı belirsiz, tüm sayıların toplamını bulma
+def toplama(*sayilar):
+  toplam = 0
+  for sayi in sayilar:
+    toplam+=sayi
+  print(toplam)
+toplama(8,5,15,25,62,58,97)
