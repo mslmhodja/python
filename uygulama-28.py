bebf429bbf230933c117d1894702632a0d525fa9
Muslim HODJA <85079865+mslmhodja@users.noreply.github.com>
2025-02-19 13:34:50 +0300

    Update uygulama-28.py

--- uygulama-28.py
+++ uygulama-28.py
@@ -35,9 +35,26 @@ print(n_toplam(5))
 
 # ödev: N sayısının faktöriyelini bulan rekürsif fonksiyon
 # ÖR: N* ..3*2*1
+def n_faktoriyel(n):
+  if n==1:
+    return 1
+  else:
+    return n * n_faktoriyel(n-1)
+
+print (n_faktoriyel(6))
 
 # ödev: 1 den N'e kadar sayıların faktöriyellerini toplayan rekürsif fonksiyon
 # ÖR: 1! + 2! + 3! + 4! + 5! + 6! + 7! ....+ N! 
+def n_faktoriyel(n):
+  if n==1:
+    return 1
+  else:
+    sonuc=1
+    for i in range(1,n+1):
+      sonuc *= i
+    return sonuc + n_faktoriyel(n-1)
+
+print (n_faktoriyel(6))
 
 # ödev: 1 den N'e kadar sayıların asal olanlarını bulup diziye aktaran rekürsif fonksiyon
 # ÖR: 1-100 arası asal sayılar 
