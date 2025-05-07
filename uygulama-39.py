3aa35c416e33d8a19d0c2dab865b271f35f1ba14
Muslim HODJA <85079865+mslmhodja@users.noreply.github.com>
2025-05-07 14:53:35 +0300

    Update uygulama-39.py

--- uygulama-39.py
+++ uygulama-39.py
@@ -21,19 +21,32 @@ def klasor_degistir():
   
 def klasor_olustur():  
     klasor = input("Oluşturmak istediğiniz klasörün adını girin: ")  
-    os.mkdir(klasor)  
-    print(f"{klasor} klasörü oluşturuldu.")  
+    if not os.path.exists(klasor):  
+        os.mkdir(klasor)  
+        print(f"{klasor} klasörü oluşturuldu.")   
+    else:  
+        print("Klasör zaten var.")  
+
   
 def klasoru_sil():  
     klasor = input("Silmek istediğiniz klasörün adını girin: ")  
-    os.rmdir(klasor)  
-    print(f"{klasor} klasörü silindi.")  
+    if os.path.exists(klasor):  
+        os.rmdir(klasor)  
+        print(f"{klasor} klasörü silindi.")   
+    else:  
+        print("Klasör bulunamadı.")  
+    
   
 def klasor_adini_degistir():  
     eski_ad = input("Eski klasör adını girin: ")  
-    yeni_ad = input("Yeni klasör adını girin: ")  
-    os.rename(eski_ad, yeni_ad)  
-    print(f"{eski_ad} klasörü {yeni_ad} olarak değiştirildi.")  
+    yeni_ad = input("Yeni klasör adını girin: ") 
+    if os.path.exists(eski_ad) and (not os.path.exists(yeni_ad)) :  
+        os.rename(eski_ad, yeni_ad)  
+        print(f"{eski_ad} klasörü {yeni_ad} olarak değiştirildi.")    
+    else:  
+        print("Klasör adı değiştirilemez.")  
+    
+ 
   
 def aktif_klasoru_goster():  
     print(f"Aktif klasör: {os.getcwd()}")  
@@ -49,20 +62,23 @@ def menu():
         print("Çıkış..........................0")  
           
         secim = input("Seçiminizi yapın: ")  
-          
-        if secim == '1':  
-            klasor_degistir()  
-        elif secim == '2':  
-            klasor_olustur()  
-        elif secim == '3':  
-            klasoru_sil()  
-        elif secim == '4':  
-            klasor_adini_degistir()  
-        elif secim == '5':  
-            aktif_klasoru_goster()  
-        elif secim == '0':   
-            break  
-        else:  
-            print("Geçersiz seçim, lütfen tekrar deneyin.")  
-  
+        try:  
+            if secim == '1':  
+                klasor_degistir()  
+            elif secim == '2':  
+                klasor_olustur()  
+            elif secim == '3':  
+                klasoru_sil()  
+            elif secim == '4':  
+                klasor_adini_degistir()  
+            elif secim == '5':  
+                aktif_klasoru_goster()  
+            elif secim == '0':   
+                break  
+            else:  
+                print("Geçersiz seçim, lütfen tekrar deneyin.")  
+        except:
+            print("bir hata oluştu")
+
 menu()  
+
