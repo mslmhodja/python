9e0593281e69862d2d4cdf99ff8249d0473e6ba3
Muslim HODJA <85079865+mslmhodja@users.noreply.github.com>
2025-05-07 13:57:25 +0300

    Create uygulama-39.py

---
+++ uygulama-39.py
@@ -0,0 +1,9 @@
+# aşağıdaki gibi bir menü oluştur
+# Klasör değiştir ........... 1
+# Klasör oluştur  ........... 2
+# Klasör Sil      ........... 3
+# Klasör Adını değiştir...... 4
+# Aktif klasörü göster....... 5
+# Çıkış ......................0
+# verilen seçenek numarasna göre işlem yap
+# her işlem için bir fonksiyon tanımla
